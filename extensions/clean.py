3f285dbb1c939da8cafeb620bc3a5b31bc7b03ef
gimme-diona <106943029+gimme-diona@users.noreply.github.com>
2022-06-06 14:25:47 +0800

    Update clean.py

--- extensions/clean.py
+++ extensions/clean.py
@@ -8,7 +8,7 @@ class Purge(Cog_Extension):
     async def purge(self,ctx,欲刪訊息數):
         limits=int(欲刪訊息數)
         await ctx.channel.send("即將開始掃地. .. ...")
-        await ctx.channel.purge(limit=limits+1)
+        await ctx.channel.purge(limit=limits+2)
         
 def setup(bot):
     bot.add_cog(Purge(bot))
